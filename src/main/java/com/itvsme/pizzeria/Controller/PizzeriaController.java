d976e538ddade04adfb3d114a42c6520ab011b81
jaca1119 <laki1663@gmail.com>
2020-01-26 12:55:43 +0100

    Added @CrossOrigin to PizzeriaController

--- src/main/java/com/itvsme/pizzeria/Controller/PizzeriaController.java
+++ src/main/java/com/itvsme/pizzeria/Controller/PizzeriaController.java
@@ -9,14 +9,12 @@ import com.itvsme.pizzeria.Service.StandardPizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class PizzeriaController
 {
     @Autowired
