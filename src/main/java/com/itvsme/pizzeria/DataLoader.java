852330074f3b7aa6d3986336e8b46b8f8958844d
jaca1119 <laki1663@gmail.com>
2020-02-10 10:11:46 +0100

    Fixed DataLoader

--- src/main/java/com/itvsme/pizzeria/DataLoader.java
+++ src/main/java/com/itvsme/pizzeria/DataLoader.java
@@ -6,10 +6,12 @@ import com.itvsme.pizzeria.Repository.StandardPizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Component
 public class DataLoader implements ApplicationRunner
 {
     private StandardPizzaRepository repository;
