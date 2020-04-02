3e38e6c79ab46b4bb52880d5c081c0a7992e25d6
jaca1119 <laki1663@gmail.com>
2020-04-02 15:22:13 +0200

    Added basic authentication and authorization

--- src/main/java/com/itvsme/pizzeria/security/WebSecurityConfig.java
+++ src/main/java/com/itvsme/pizzeria/security/WebSecurityConfig.java
@@ -4,53 +4,60 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 {
+    @Bean
+    BCryptPasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
         http
+                .cors(Customizer.withDefaults())
+                .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers(HttpMethod.GET,"/standard/**", "/addons/**", "/sizes").permitAll()
-                    .antMatchers(HttpMethod.POST, "/order-pizza-cart").permitAll()
-                    .antMatchers(HttpMethod.OPTIONS, "/order-pizza-cart").permitAll()
-                    .antMatchers("/all-orders").hasRole("ADMIN")
-                    .anyRequest().hasRole("ADMIN")
-                    .and()
-                .csrf().disable();
+                .antMatchers(HttpMethod.GET, "/standard/**", "/addons/**", "/sizes").permitAll()
+                .antMatchers(HttpMethod.POST, "/order-pizza-cart").permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/all-orders").hasRole("ADMIN")
+                .anyRequest().hasRole("ADMIN")
+                .and()
+                .httpBasic();
     }
 
     @Bean
-    @Override
-    protected UserDetailsService userDetailsService()
+    public CorsConfigurationSource corsConfigurationSource()
     {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration());
 
-        UserDetails user =
-                User.builder()
-                    .username("qwe")
-                    .password(encoder.encode("123"))
-                    .roles("USER")
-                    .build();
-        UserDetails admin =
-                User.builder()
-                        .roles("USER", "ADMIN")
-                        .username("admin")
-                        .password(encoder.encode("admin"))
-                        .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+        return source;
     }
 }
